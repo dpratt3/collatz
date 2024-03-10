077d0cf9d587dffaa68ef3376c9ec65541f0967f
David <david@midnightmechanism.com>
2024-03-10 15:59:31 -0500

    Move script to scripts dir

--- scripts/collatz.py
+++ scripts/collatz.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 
 def collatz(num):
@@ -5,7 +6,7 @@ def collatz(num):
    
     while num > 1:
         if num % 2 == 0:
-            num = num // 2 # Handle integer div. for very large num
+            num = num // 2 # Force integer div. for very large num
         else:
             num = 3 * num + 1
         values.append(num)
@@ -17,10 +18,11 @@ def main():
     parser.add_argument('number', type=int, help='The starting number for the Collatz Conjecture')
 
     args = parser.parse_args()
+    
     sequence = collatz(args.number)
 
     print("Length:", len(sequence))
-    print("Sequence:", sequence)
+    # print("Sequence:", sequence)
 
 if __name__ == "__main__":
     main()
